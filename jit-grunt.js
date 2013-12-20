459ae289d96e2e9d0a6139f6eabc7e53150055a3
shootaroo <shotaro.tsubouchi@gmail.com>
2013-12-21 00:07:37 +0900

    Auto mapping plugin name grunt-contrib-*, grunt-* and *

--- jit-grunt.js
+++ jit-grunt.js
@@ -1,23 +1,42 @@
 'use strict';
-var _ = require('lodash');
+module.exports = function (grunt, plugins) {
+  var path = require('path');
+  var _ = require('lodash');
 
-module.exports = function (grunt) {
-
-  var plugins = grunt.file.readJSON('.gruntplugins');
+  var PREFIXES = ['', 'grunt-', 'grunt-contrib-'];
   var loaded = [];
+  var root = path.resolve('node_modules');
+
+
+  var loadPlugin = function (name) {
+    var tasksDir = path.join(root, name, 'tasks');
+    if (grunt.file.exists(tasksDir)) {
+      grunt.log.write('\nLoading "' + name + '" plugin...');
+      grunt.loadTasks(tasksDir);
+      grunt.log.ok();
+      return true;
+    }
+    return false;
+  };
+
+  plugins = plugins || {};
 
   var run = grunt.task.run;
   grunt.task.run = function (tasks) {
-    grunt.log.debug('task.run:', tasks);
     tasks = _.isArray(tasks) ? tasks : [tasks];
-    _.each(tasks, function (name) {
-      var taskName = name.split(':')[0];
-      var plugin = plugins[taskName];
-      if (plugin && !loaded[plugin]) {
-        grunt.log.writeln('Loading "' + plugin + '" plugin...');
-        grunt.loadNpmTasks(plugin);
-        loaded[plugin] = true;
+    tasks.forEach(function (task) {
+      var taskName = task.split(':')[0];
+      if (loaded[taskName]) {
+        return;
       }
+      if (!plugins[taskName] || !loadPlugin(plugins[taskName])) {
+        for (var i = PREFIXES.length; i--;) {
+          if (loadPlugin(PREFIXES[i] + taskName)) {
+            break;
+          }
+        }
+      }
+      loaded[taskName] = true;
     });
     run.apply(grunt.task, arguments);
   };
