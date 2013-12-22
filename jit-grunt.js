c83f1e0c1d26ceb5c10b9349f35e42750a1c6279
shinnn <dminor.sw@gmail.com>
2013-12-22 22:47:11 +0900

    use 'hasOwnProperty' to check if loaded

--- jit-grunt.js
+++ jit-grunt.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = function (grunt, plugins) {
   var path = require('path');
-  var _ = require('lodash');
 
   var PREFIXES = ['', 'grunt-', 'grunt-contrib-'];
   var loaded = [];
@@ -23,13 +22,13 @@ module.exports = function (grunt, plugins) {
 
   var run = grunt.task.run;
   grunt.task.run = function (tasks) {
-    tasks = _.isArray(tasks) ? tasks : [tasks];
+    tasks = Array.isArray(tasks) ? tasks : [tasks];
     tasks.forEach(function (task) {
       var taskName = task.split(':')[0];
-      if (loaded[taskName]) {
+      if (loaded.hasOwnProperty(taskName)) {
         return;
       }
-      if (!plugins[taskName] || !loadPlugin(plugins[taskName])) {
+      if (!plugins.hasOwnProperty(taskName) || !loadPlugin(plugins[taskName])) {
         for (var i = PREFIXES.length; i--;) {
           if (loadPlugin(PREFIXES[i] + taskName)) {
             break;
