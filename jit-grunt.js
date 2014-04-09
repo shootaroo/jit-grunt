ed79fa144972fc57754031e85b332db956a9002e
shootaroo <shotaro.tsubouchi@gmail.com>
2014-04-09 23:21:26 +0900

    Using path.resolve #14

--- jit-grunt.js
+++ jit-grunt.js
@@ -1,10 +1,14 @@
 'use strict';
 var jitGrunt = require('./lib/jit-grunt');
+var path = require('path');
 
 module.exports = function (grunt, mappings) {
   var jit = jitGrunt(grunt, mappings);
   return function (options) {
     options = options || {};
-    jit.loadTasks = options.loadTasks;
+
+    if (options.loadTasks) {
+      jit.loadTasks = path.resolve(options.loadTasks);
+    }
   };
 };
--- test/findPluginSpec.js
+++ test/findPluginSpec.js
@@ -1,5 +1,6 @@
 /* global describe, beforeEach, it */
 'use strict';
+var path = require('path');
 var assert = require('power-assert');
 var grunt = require('grunt');
 var jit = require('../lib/jit-grunt')(grunt);
@@ -15,88 +16,88 @@ describe('Plugin find', function() {
   });
 
   it('grunt-contrib-foo', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo/tasks').returns(true);
-    stub.withArgs('node_modules/grunt-foo/tasks').returns(false);
-    stub.withArgs('node_modules/foo/tasks').returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo/tasks')).returns(true);
+    stub.withArgs(path.resolve('node_modules/grunt-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/foo/tasks')).returns(false);
 
     assert(jit.findPlugin('foo'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo/tasks'));
-    assert(!stub.calledWith('node_modules/grunt-foo/tasks'));
-    assert(!stub.calledWith('node_modules/foo/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/grunt-foo/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/foo/tasks')));
   });
 
   it('grunt-foo', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo/tasks').returns(false);
-    stub.withArgs('node_modules/grunt-foo/tasks').returns(true);
-    stub.withArgs('node_modules/foo/tasks').returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-foo/tasks')).returns(true);
+    stub.withArgs(path.resolve('node_modules/foo/tasks')).returns(false);
     stub.returns(false);
 
     assert(jit.findPlugin('foo'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo/tasks'));
-    assert(stub.calledWith('node_modules/grunt-foo/tasks'));
-    assert(!stub.calledWith('node_modules/foo/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-foo/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/foo/tasks')));
   });
 
   it('foo', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo/tasks').returns(false);
-    stub.withArgs('node_modules/grunt-foo/tasks').returns(false);
-    stub.withArgs('node_modules/foo/tasks').returns(true);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/foo/tasks')).returns(true);
 
     assert(jit.findPlugin('foo'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo/tasks'));
-    assert(stub.calledWith('node_modules/grunt-foo/tasks'));
-    assert(stub.calledWith('node_modules/foo/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-foo/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/foo/tasks')));
   });
 
   it('fooBar', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo-bar/tasks').returns(false);
-    stub.withArgs('node_modules/grunt-foo-bar/tasks').returns(true);
-    stub.withArgs('node_modules/foo-bar/tasks').returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo-bar/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-foo-bar/tasks')).returns(true);
+    stub.withArgs(path.resolve('node_modules/foo-bar/tasks')).returns(false);
 
     assert(jit.findPlugin('fooBar'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo-bar/tasks'));
-    assert(stub.calledWith('node_modules/grunt-foo-bar/tasks'));
-    assert(!stub.calledWith('node_modules/foo-bar/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo-bar/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-foo-bar/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/foo-bar/tasks')));
   });
 
   it('foo_bar', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo-bar/tasks').returns(false);
-    stub.withArgs('node_modules/grunt-foo-bar/tasks').returns(false);
-    stub.withArgs('node_modules/foo-bar/tasks').returns(true);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo-bar/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-foo-bar/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/foo-bar/tasks')).returns(true);
 
     assert(jit.findPlugin('foo_bar'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo-bar/tasks'));
-    assert(stub.calledWith('node_modules/grunt-foo-bar/tasks'));
-    assert(stub.calledWith('node_modules/foo-bar/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo-bar/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-foo-bar/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/foo-bar/tasks')));
   });
 
   it('Custom task', function () {
-    jit.loadTasks = 'custom';
+    jit.loadTasks = path.resolve('custom');
 
-    stub.withArgs('custom/foo.js').returns(true);
+    stub.withArgs(path.resolve('custom/foo.js')).returns(true);
 
     assert(jit.findPlugin('foo'));
 
-    assert(stub.calledWith('custom/foo.js'));
-    assert(!stub.calledWith('node_modules/grunt-contrib-foo/tasks'));
-    assert(!stub.calledWith('node_modules/grunt-foo/tasks'));
-    assert(!stub.calledWith('node_modules/foo/tasks'));
+    assert(stub.calledWith(path.resolve('custom/foo.js')));
+    assert(!stub.calledWith(path.resolve('node_modules/grunt-contrib-foo/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/grunt-foo/tasks')));
+    assert(!stub.calledWith(path.resolve('node_modules/foo/tasks')));
   });
 
   it('not Found', function () {
-    stub.withArgs('node_modules/grunt-contrib-foo/tasks').returns(false);
-    stub.withArgs('node_modules/grunt-foo/tasks').returns(false);
-    stub.withArgs('node_modules/foo/tasks').returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-contrib-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/grunt-foo/tasks')).returns(false);
+    stub.withArgs(path.resolve('node_modules/foo/tasks')).returns(false);
 
     assert(!jit.findPlugin('foo'));
 
-    assert(stub.calledWith('node_modules/grunt-contrib-foo/tasks'));
-    assert(stub.calledWith('node_modules/grunt-foo/tasks'));
-    assert(stub.calledWith('node_modules/foo/tasks'));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-contrib-foo/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/grunt-foo/tasks')));
+    assert(stub.calledWith(path.resolve('node_modules/foo/tasks')));
   });
 });
